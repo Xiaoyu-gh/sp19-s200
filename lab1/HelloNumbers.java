2af825ffe060e0da6c643af863518a65e61cfa0b
Shirley Zhou <shirleyzhou428@berkeley.edu>
2019-01-24 23:27:39 -0800

    HelloNumbers 3rd revision

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,7 +1,7 @@
 public class HelloNumbers {
 	public static void main(String[] args) {
 		int x = 0;
-		int count = 0';
+		int count = 0;
 		while (x < 45) {
 			x = x + count;
 			System.out.print(x + " ");
