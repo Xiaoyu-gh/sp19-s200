2aa1f73bc57c870e7a9bbf99fd9101b109fdc7f1
Shirley Zhou <shirleyzhou428@berkeley.edu>
2019-03-01 10:25:39 -0800

    Hw1 task 1&2

--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -1,11 +1,12 @@
 package es.datastructur.synthesizer;
+import java.lang.reflect.Array;
 import java.util.Iterator;
 
 //TODO: Make sure to that this class and all of its methods are public
 //TODO: Make sure to add the override tag for all overridden methods
 //TODO: Make sure to make this class implement BoundedQueue<T>
 
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> implements BoundedQueue<T>{
     /* Index for the next dequeue or peek. */
     private int first;
     /* Index for the next enqueue. */
@@ -21,15 +22,37 @@ public class ArrayRingBuffer<T>  {
     public ArrayRingBuffer(int capacity) {
         // TODO: Create new array with capacity elements.
         //       first, last, and fillCount should all be set to 0.
+
+        rb = (T[]) new Object [capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+
     }
+
+    @Override
+    public int capacity() {
+        return rb.length;
+    }
+
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
+
 
     /**
      * Adds x to the end of the ring buffer. If there is no room, then
      * throw new RuntimeException("Ring buffer overflow").
      */
+    @Override
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update
         //       last.
+        if (isFull()){
+            throw new RuntimeException("Ring buffer overflow!!");
+        }
+
         return;
     }
 
@@ -40,6 +63,9 @@ public class ArrayRingBuffer<T>  {
     public T dequeue() {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and
         //       update first.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow!!");
+        }
         return null;
     }
 
---
+++ hw1/es/datastructur/synthesizer/BoundedQueue.java
@@ -0,0 +1,23 @@
+package es.datastructur.synthesizer;
+
+public interface BoundedQueue <T>{
+
+
+    int capacity();
+    int fillCount();
+    void enqueue(T x);
+    T dequeue();
+    T peek();
+    default boolean isEmpty() {
+        if (fillCount() == 0) {
+            return true;
+        }
+        return false;
+    };
+    default boolean isFull() {
+        if (fillCount() == capacity()) {
+            return true;
+        }
+        return false;
+    }
+}
