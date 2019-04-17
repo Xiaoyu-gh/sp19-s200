df73c1f3508bd7cd9bcf58e7f3ae4adfb318d722
Shirley Zhou <shirleyzhou428@berkeley.edu>
2019-04-17 12:52:40 -0700

    proj2c part 3 first try

--- proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
+++ proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
@@ -6,7 +6,9 @@ import bearmaps.hw4.streetmap.StreetMapGraph;
 import bearmaps.proj2ab.Point;
 import bearmaps.proj2ab.PointSet;
 import bearmaps.proj2ab.WeirdPointSet;
+import org.apache.commons.math3.linear.ArrayRealVector;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -21,32 +23,44 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
     private List<Point> coordinates;
     private WeirdPointSet tree;
     private HashMap<Point, Node> map;
+    private MyTrieSet placeNames;
+    private HashMap<String, List<Node>> placeInfo;
 
     public AugmentedStreetMapGraph(String dbPath) {
-         super(dbPath);
+        super(dbPath);
 //         You might find it helpful to uncomment the line below:
-         List<Node> nodes = this.getNodes();
-         coordinates = new ArrayList<>();
-         map = new HashMap<>();
-         for (int i = 0; i < nodes.size(); i++) {
-             Node currNode = nodes.get(i);
-             Point currPoint = new Point(currNode.lon(), currNode.lat());
-             if (this.neighbors(currNode.id()).size() != 0) {
-                 coordinates.add(currPoint);
-                 map.put(currPoint, currNode);
-             }
-         }
+        List<Node> nodes = this.getNodes();
+        coordinates = new ArrayList<>();
+        map = new HashMap<>();
+        placeNames = new MyTrieSet();
+        placeInfo = new HashMap<>();
 
-//         for (int i = 0; i < nodes.size(); i++) {
-//             Node currNode = nodes.get(i);
-//             Point currPoint = new Point(currNode.lon(), currNode.lat());
-//             if (this.neighbors(currNode.id()).size() == 0) {
-//                 coordinates.remove(currPoint);
-//                 map.remove(currPoint);
-//             }
-//         }
+        for (int i = 0; i < nodes.size(); i++) {
+            Node currNode = nodes.get(i);
+            Point currPoint = new Point(currNode.lon(), currNode.lat());
+            if (this.neighbors(currNode.id()).size() != 0) {
+                coordinates.add(currPoint);
+                map.put(currPoint, currNode);
+            }
+
+            if (currNode.name() != null) {
+                String currName = currNode.name();
+                if (placeNames.contains(currName)) {
+                    placeNames.add(currName);
+                    List<Node> info = new ArrayList<>();
+                    placeInfo.put(currName, info);
+                }
+                List<Node> info = placeInfo.get(currName);
+                info.add(currNode);
+                placeInfo.put(currName, info);
+            }
+        }
+
+
+
          tree = new WeirdPointSet(coordinates);
     }
+
 
 
     /**
@@ -57,21 +71,6 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * @return The id of the node in the graph closest to the target.
      */
     public long closest(double lon, double lat) {
-//        Point currPoint = new Point(lon, lat);
-//        Node currNode = map.get(currPoint);
-//        long currLong = currNode.id();
-//        List<WeightedEdge<Long>> neighbors = this.neighbors(currLong);
-//
-//        List<Point> neighborPoints = new ArrayList<>();
-//        neighborPoints.add(currPoint);
-//        for (WeightedEdge<Long> e : neighbors) {
-//            Long l = e.to();
-//            neighborPoints.add(new Point(this.lon(l), this.lat(l)));
-//        }
-//
-//        WeirdPointSet neighborTree = new WeirdPointSet(neighborPoints);
-//
-//        Point closestPoint = neighborTree.nearest(lon, lat);
 
         Point closestPoint = tree.nearest(lon, lat);
         return map.get(closestPoint).id();
@@ -87,7 +86,8 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * cleaned <code>prefix</code>.
      */
     public List<String> getLocationsByPrefix(String prefix) {
-        return new LinkedList<>();
+        String pre = cleanString(prefix);
+        return placeNames.keysWithPrefix(pre);
     }
 
     /**
@@ -104,7 +104,23 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * "id" -> Number, The id of the node. <br>
      */
     public List<Map<String, Object>> getLocations(String locationName) {
+        String locName = cleanString(locationName);
+        List<Map<String, Object>> locs = new ArrayList<>();
+        List<Node> nodes = placeInfo.get(locName);
+        for (Node n : nodes) {
+            locs.add(infoHelper(n));
+        }
+
         return new LinkedList<>();
+    }
+
+    private Map<String, Object> infoHelper(Node n) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("lat", n.lat());
+        result.put("lon", n.lon());
+        result.put("name", n.name());
+        result.put("id", n.id());
+        return result;
     }
 
 
@@ -117,5 +133,125 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
     private static String cleanString(String s) {
         return s.replaceAll("[^a-zA-Z ]", "").toLowerCase();
     }
+
+
+    private class MyTrieSet {
+
+        private Node root;
+
+        private class Node {
+            char value;
+            boolean atEnd;
+            HashMap<Character, Node> kids;
+
+            Node(char c) {
+                this.value = c;
+                this.atEnd = false;
+                this.kids = new HashMap<>();
+            }
+        }
+
+        public MyTrieSet() {
+
+            this.root = new Node('\0');
+        }
+
+
+        public void clear() {
+            this.root = new Node('\0');
+
+        }
+
+        public boolean contains(String key) {
+            if (key == null || key.length() == 0) {
+                return true;
+            }
+
+            Node currNode = root;
+            for (int i = 0; i < key.length(); i++) {
+                char curr = key.charAt(i);
+                // if the current node has this letter, then go to that node and
+                // don't create new node
+                if (!currNode.kids.containsKey(curr)) {
+                    return false;
+                }
+                currNode = currNode.kids.get(curr);
+            }
+            return currNode.atEnd;
+        }
+
+        public void add(String key) {
+            if (key == null || key.length() == 0) {
+                return;
+            }
+            Node currNode = root;
+            for (int i = 0; i < key.length(); i++) {
+                char curr = key.charAt(i);
+                // if the current node has this letter, then go to that node and
+                // don't create new node
+                if (!currNode.kids.containsKey(curr)) {
+                    Node newNode = new Node(curr);
+                    currNode.kids.put(curr, newNode);
+                }
+                currNode = currNode.kids.get(curr);
+            }
+            currNode.atEnd = true;
+        }
+
+        public List<String> keysWithPrefix(String prefix) {
+            LinkedList<String> results = new LinkedList<>();
+            //traverse the trie until reaching the end;
+            if (prefix == null || prefix.length() == 0) {
+                return results;
+            }
+
+            Node currNode = root;
+            for (int i = 0; i < prefix.length(); i++) {
+                char curr = prefix.charAt(i);
+                // if the current node has this letter, then go to that node and
+                // don't create new node
+                if (!currNode.kids.containsKey(curr)) {
+                    return results;
+                }
+                currNode = currNode.kids.get(curr);
+            }
+
+            prefixHelper(currNode, prefix, results);
+            return results;
+        }
+
+        private void prefixHelper(Node curr, String prefix, LinkedList<String> results) {
+            if (curr.atEnd) {
+                results.add(prefix);
+            }
+            for (Character c : curr.kids.keySet()) {
+                Node kid = curr.kids.get(c);
+                prefixHelper(kid, prefix + c, results);
+            }
+        }
+
+        public String longestPrefixOf(String key) {
+            String result = "";
+            if (key == null || key.length() == 0) {
+                return result;
+            }
+
+            Node currNode = root;
+            for (int i = 0; i < key.length(); i++) {
+                char curr = key.charAt(i);
+                // if the current node has this letter, then go to that node and
+                // don't create new node
+                if (!currNode.kids.containsKey(curr)) {
+                    return result;
+                }
+                result = result + Character.toString(curr);
+                currNode = currNode.kids.get(curr);
+            }
+            return result;
+        }
+    }
+
+
+
 
 }
